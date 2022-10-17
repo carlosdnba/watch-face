4e91b1c776bb04c736c44ea205c39309daea1ca4
Carlos Daniel <carlosdnbarboza@gmail.com>
2022-10-17 18:34:57 +0100

    Initial Commit

---
+++ WatchFace/WatchFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchFaceApp.swift
+//  WatchFace
+//
+//  Created by Carlos on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
