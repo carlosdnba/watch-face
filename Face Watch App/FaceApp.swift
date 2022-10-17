9e57017984cb53b3e72bc16fa4d7bc88a301d440
Carlos Daniel <carlosdnbarboza@gmail.com>
2022-10-17 18:39:14 +0100

    feat: add watch app

---
+++ Face Watch App/FaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceApp.swift
+//  Face Watch App
+//
+//  Created by Carlos on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Face_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
